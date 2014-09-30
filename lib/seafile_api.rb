e958bba414fafadf8486602f3f5aab9acb1399b4
Sean Cleveland <sean.b.cleveland@gmail.com>
2014-09-30 14:36:35 -0600

    fix token get

--- lib/seafile_api.rb
+++ lib/seafile_api.rb
@@ -5,15 +5,15 @@ require 'json'
 class Seafile
 
 
-	def self.token(base_url, username, password)
+	def self.get_token(base_url, username, password)
 	  response = Curl::Easy.http_post(base_url+'/api2/auth-token/',Curl::PostField.content('username', username),Curl::PostField.content('password', password))do |c|
 	    c.ssl_verify_peer = false 
 	  end
-	  if JSON.parse(response.body)
+	  #if JSON.parse(response.body)
 	    JSON.parse(response.body)['token']
-	  else
-	  	response.body
-	  end
+	  #else
+	 ## 	response.body
+	  #end
 	end
 
 	
