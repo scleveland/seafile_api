74ce97a0b35fc63a901374eebca76b9a4306c05c
Sean Cleveland <sean.b.cleveland@gmail.com>
2014-09-30 14:43:07 -0600

    add sea file initializer

--- lib/seafile_api.rb
+++ lib/seafile_api.rb
@@ -4,13 +4,24 @@ require 'json'
 
 class Seafile
 
+	attr_accessor :username
+	attr_accessor :password
+	attr_accessor :host
+	attr_accessor :token
 
-	def self.get_token(base_url, username, password)
+    def initialize(host,username,password)
+     self.username = username
+	 self.password = password
+	 self.host = host
+	 self.token = self.get_token(sefl.host, selfusername, self.password)
+    end
+
+	def self.get_token(host, username, password)
 	  response = Curl::Easy.http_post(base_url+'/api2/auth-token/',Curl::PostField.content('username', username),Curl::PostField.content('password', password))do |c|
 	    c.ssl_verify_peer = false 
 	  end
 	  #if JSON.parse(response.body)
-	    JSON.parse(response.body)['token']
+	  JSON.parse(response.body)['token']
 	  #else
 	 ## 	response.body
 	  #end
